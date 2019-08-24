b1152bd65c8541aa7c8b486f3ee59bf2dfceff62
Ariana <ariana.villegas@utec.edu.pe>
2019-08-24 08:03:42 -0500

    Resuelto

--- main.cpp
+++ main.cpp
@@ -2,6 +2,25 @@
 
 using namespace std;
 
-int main(int argc, char *argv[]) {
-    return 0;
+bool first=1;
+
+void newLine(char letter){
+    if (letter == '"'){
+        if (first){
+            cout << "``";
+        } else {
+            cout << "''";
+        }
+        first = !first;
+    } else {
+        cout << letter;
+    }
 }
+
+int main(int argc, char *argv[]) {
+    char letter;
+    while(scanf("%c",&letter)==1){
+        newLine(letter);
+    }
+}
+
